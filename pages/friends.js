d584ee35fb2c76f3f7b0edd2ad0b4d3b9b522f2c
usama508 <musama023418@gmail.com>
2023-05-09 00:40:59 +0500

    ok

--- pages/friends.js
+++ pages/friends.js
@@ -105,7 +105,7 @@ export default function friends() {
                         className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                         onClick={() => setSidebarOpen(false)}
                       >
-                        <span className="sr-only">Close sidebar</span>
+                        <span className="sr-only">Close sidebars</span>
                         <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
                       </button>
                     </div>
